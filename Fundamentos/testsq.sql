3ade92b3aa29cb92284484b116081e8309c1d1db
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-01-13 14:33:53 -0300

    Testar nova Tabela

---
+++ Fundamentos/testsq.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Tb_Pessoa
+(
+	Pessoa_Id INT PRIMARY KEY,
+	Nome_Fk INT FOREIGN KEY REFERENCES Tb_Pessoa (Pessoa_Id) NOT NULL,
+	Cidade VARCHAR (50) UNIQUE,
+	Idade INT CHECK (Idade > 18),
+	Data DATE DEFAULT GETDATE()
+)

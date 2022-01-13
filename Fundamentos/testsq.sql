9312926e276cd049741c5e21765cc004701ee2dd
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-01-13 15:04:32 -0300

    Select

--- Fundamentos/testsq.sql
+++ Fundamentos/testsq.sql
@@ -3,3 +3,7 @@ SET @STRING_TRIM = '   7 espaço após essa frase.';
 SELECT @STRING_TRIM + 'Proxima string.' AS Com_Espaço;
 
 SELECT LTRIM (@STRING_TRIM)+ 'Proxima string.' AS Sem_Espaço;
+
+SELECT TOP 10
+UPPER(FirstName) FirstName
+FROM [Person].[Person]
--- Fundamentos/testsq.sql.bak
+++ Fundamentos/testsq.sql.bak
@@ -1,1 +1,5 @@
 DECLARE @STRING_TRIM VARCHAR(60);
+SET @STRING_TRIM = '   7 espaço após essa frase.';
+SELECT @STRING_TRIM + 'Proxima string.' AS Com_Espaço;
+
+SELECT LTRIM (@STRING_TRIM)+ 'Proxima string.' AS Sem_Espaço;

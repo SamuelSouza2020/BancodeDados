d3ca810630bdc6e0ef5f0f4fbc515f73c2f67fc9
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-01-13 14:41:30 -0300

    Nova tabela

--- Fundamentos/testsq.sql
+++ Fundamentos/testsq.sql
@@ -1,11 +1,5 @@
-CREATE TABLE Tb_Pessoa
-(
-	Pessoa_Id INT PRIMARY KEY,
-	Nome_Fk INT FOREIGN KEY REFERENCES Tb_Pessoa (Pessoa_Id) NOT NULL,
-	Cidade VARCHAR (50) UNIQUE,
-	Idade INT CHECK (Idade > 18),
-	Data DATE DEFAULT GETDATE()
-)
-
-SELECT SUBSTRING(FirstName, 1, 1), LastName, FirstName
-	FROM Person.Person;
+SELECT Name,
+	Replicate ('0', 4) + [ProductLine] AS 'Code Line'
+	FROM [Production].[Produtct]
+	WHERE [productLine] = 'T'
+	ORDER BY [Name];
